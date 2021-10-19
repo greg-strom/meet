5ff97658330e31790c80a8431693090f195a27b1
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-10-19 15:02:47 -0400

    Got the getCalendarEvents function working

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -106,8 +106,6 @@ module.exports.getAccessToken = async (event) => {
     });
 }
 
-// note to self: I just did step 4. not sure I did step 3 correctly!
-
 module.exports.getCalendarEvents = async (event) => {
   // The values used to instantiate the OAuthClient are at the top of the file
   const oAuth2Client = new google.auth.OAuth2(
@@ -115,31 +113,35 @@ module.exports.getCalendarEvents = async (event) => {
     client_secret,
     redirect_uris[0]
   );
-  // Decode authorization code extracted from the URL query
-  const access_token = decodeURIComponent(`${event.pathParameters.code}`);
+
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
   oAuth2Client.setCredentials({ access_token });
 
   return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-     */
-
-    oAuth2Client.getToken(access_token, (err, token) => {
-      if (err) {
-        return reject(err);
+     calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
       }
-      return resolve(token);
-    });
+    );
     })
-    .then((token) => {
-      // Respond with OAuth token 
+    .then((results) => {
       return {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify(token),
+        body: JSON.stringify({ events: results.data.items }),
       };
     })
     .catch((err) => {
