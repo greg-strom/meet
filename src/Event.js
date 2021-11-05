e50e5f297012f0e04146446a69b714c1acb8eb44
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-11-05 12:01:58 -0400

    made an initial attempt to get Event.js in order

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
+  state = {
+    display: false,
+  };
+
+  handleButton = () => {
+    this.setState((prevState) => ({ display: !prevState.display }));
+  };
+
   render() {
-    return <div></div>;
+    let event = this.props.event;
+
+    return( 
+      <div className="event">
+        <ul>
+          <li className="EventSummary">{event.summary}</li>
+          <li className="EventLocation">{event.location}</li>
+          <li className="EventTime">{event.start.dateTime} - {event.start.timeZone}</li>
+          {this.state.display === true && (
+            <p className="EventDescription">{event.description}</p>
+          )}
+          {this.state.display === false && (
+            <button className="displayMore" onClick={() => this.handleButton()}>
+              Show details
+            </button>
+          )}
+          {this.state.display === true && (
+            <button className="displayLess" onClick={() => this.handleButton()}>
+              Hide details
+            </button>
+          )}
+        </ul>
+      </div>
+    )
+
   }
+
 }
+
 export default Event;
