e3fc7b8b87fa35071f2fb58044b8692e12d5c772
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-11-05 12:12:55 -0400

    Got tests working for display of basic event info

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -11,6 +11,16 @@ describe('<Event /> component', () => {
     EventWrapper = shallow(<Event event={mockData[1]} />);
   });
 
-  
+  test("Summary is displayed", () => {
+    expect(EventWrapper.find(".EventSummary")).toHaveLength(1);
+  });
+
+  test("Location is displayed", () => {
+    expect(EventWrapper.find(".EventLocation")).toHaveLength(1);
+  });
+
+  test("Time and timezone are displayed", () => {
+    expect(EventWrapper.find(".EventTime")).toHaveLength(1);
+  });
 
 });
