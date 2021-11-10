57aac0370269899927c1f85d00a9c31ac6f569e1
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-11-10 10:29:50 -0500

    got test working to confirm NumberOfEvents exists

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,7 @@ import App from '../App';
 import EventList from '../EventList';
 import Event from '../Event';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
   let AppWrapper;
@@ -18,4 +19,8 @@ describe('<App /> component', () => {
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
+
+  test('render number of events', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  })
 });
