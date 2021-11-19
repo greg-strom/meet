1351a95786d3c3f9acc9effd37e022fa947e3256
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-11-19 11:49:04 -0500

    added code to send user w/o token to Google API

--- src/api.js
+++ src/api.js
@@ -22,4 +22,20 @@ import axios from 'axios';
 
   export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "https://4h41ev44c7.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+    }
+    return accessToken;
   }
