b332f5be8518a4285efdc4f6b949007afae29871
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-11-19 11:58:00 -0500

    added code to make GET request to calendar API

--- src/api.js
+++ src/api.js
@@ -27,11 +27,28 @@ import axios from 'axios';
   };
 
   export const getEvents = async () => {
-    if (window.location.href.startsWith('http://localhost')) {
+    NProgress.start();
+  
+    if (window.location.href.startsWith("http://localhost")) {
+      NProgress.done();
       return mockData;
     }
-  };
 
+    const token = await getAccessToken();
+  
+    if (token) {
+      removeQuery();
+      const url = 'https://4h41ev44c7.execute-api.us-east-1.amazonaws.com/dev/api/get-events/' + token;
+      const result = await axios.get(url);
+      if (result.data) {
+        var locations = extractLocations(result.data.events);
+        localStorage.setItem("lastEvents", JSON.stringify(result.data));
+        localStorage.setItem("locations", JSON.stringify(locations));
+      }
+      NProgress.done();
+      return result.data.events;
+    }
+  };
   export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
     const tokenCheck = accessToken && (await checkToken(accessToken));
