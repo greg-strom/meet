6b5666015e6f1454c45f7d04b0409df069c7bd3e
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-12-15 11:55:52 -0500

    changed location of NumberOf Events box

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ class App extends Component {
   return (
     <div className="App">
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-      <EventList events={this.state.events} />
       <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
+      <EventList events={this.state.events} />
     </div>
   );
   }
