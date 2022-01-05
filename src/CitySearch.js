69e6db6c9ac176763d16cdd61e441df05d49bae2
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-05 13:27:20 -0500

    moved the alert/error text to the side

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -18,7 +18,7 @@ class CitySearch extends Component {
     if (suggestions.length === 0) {
       this.setState({
         query: value,
-        infoText: 'We can not find the city you are looking for. Please try another city',
+        infoText: 'We can not find the city you are looking for. Please try another city.',
       });
     } else {
       return this.setState({
@@ -40,24 +40,29 @@ class CitySearch extends Component {
 
   render() {
     return (
-      <div className="CitySearch">
-        <InfoAlert text={this.state.infoText} />
-        <input
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          onFocus={() => { this.setState({ showSuggestions: true }) }}
-        />
-        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
-          {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
-          ))}
-          <li onClick={() => this.handleItemClicked("all")}>
-          <b>See all the cities</b>
-        </li>
-        </ul>
+      <div>
+        <div className="CitySearch">
+          <input
+            type="text"
+            className="city"
+            value={this.state.query}
+            onChange={this.handleInputChanged}
+            onFocus={() => { this.setState({ showSuggestions: true }) }}
+          />
+          <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+            {this.state.suggestions.map((suggestion) => (
+              <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
+            ))}
+            <li onClick={() => this.handleItemClicked("all")}>
+            <b>See all the cities</b>
+          </li>
+          </ul>
+        </div>
+        <div className="citySearchAlert">
+          <InfoAlert text={this.state.infoText} />
+        </div>
       </div>
+
     );
   }
 }
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -11,7 +11,7 @@ class NumberOfEvents extends Component {
     this.props.updateNumberOfEvents(value);
     if (!(value > 0 && value < 65)) {
       this.setState({
-        errorText: 'Please enter a number between 1 and 64',
+        errorText: 'Please enter a number between 1 and 64.',
       });
     } else {
       return this.setState({
@@ -23,14 +23,18 @@ class NumberOfEvents extends Component {
 
   render() {
     return (
-      <div className="numberOfEvents">
-        <input
-          type="text"
-          className="EventsNumberTextbox"
-          value={this.props.numberOfEvents}
-          onChange={(e) => this.handleNumberOfEventsChange(e)}
-        />
-        <ErrorAlert text={this.state.errorText} />
+      <div>
+        <div className="numberOfEvents">
+          <input
+            type="text"
+            className="EventsNumberTextbox"
+            value={this.props.numberOfEvents}
+            onChange={(e) => this.handleNumberOfEventsChange(e)}
+          />
+        </div>
+        <div className="numberOfEventsError">
+          <ErrorAlert text={this.state.errorText} />
+        </div>
       </div>
     );
   }
