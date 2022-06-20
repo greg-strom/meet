9fe28b70288692eb9c2d8565cba41ea7554bee12
greg-strom <greg.b.strom@gmail.com>
2022-06-20 12:57:07 -0400

    changed gbstrom's to greg-strom

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -29,7 +29,7 @@ function WelcomeScreen(props) {
                     </div>
                 </div>
                 <a
-                    href="https://gbstrom.github.io/meet/privacy.html"
+                    href="https://greg-strom.github.io/meet/privacy.html"
                     rel="nofollow noopener"
                 >
                     Privacy policy
